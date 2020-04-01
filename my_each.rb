26176468ab35800375a6cc152a737d8c7ce7c559
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-01 04:56:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,23 @@ def my_each(array)
  n=array.size
  u=0
  while o<n
- yield #{array[u]}
+ yield array[u]
  u=u+1
  end
 return array
 end
+
+def hello_t(array)
+  if block_given?
+    i=0
+    while i<array.length
+    yield array[i]
+    i = i+1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+# call 
