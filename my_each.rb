5f55c5da8332adca96d606998d5450e1759de486
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-01 04:55:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,24 +1,9 @@
 def my_each(array)
  n=array.size
- o=0
+ u=0
  while o<n
- "#{array[o]}"
- o+=1
+ yield #{array[u]}
+ u=u+1
  end
 return array
 end
-
-def hello_t(array)
-  if block_given?
-    i=0
-    while i<array.length
-    yield array[i]
-    i = i+1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
-
-# call 
